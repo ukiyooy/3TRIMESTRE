c953cb65de692f7582c69792bf9a6807421200d2
maria <114171268+ukiyooy@users.noreply.github.com>
2023-10-30 07:47:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+function tocasSomPom () {
+document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom');
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador= 0;
+
+//enquanto
+while (contador < listaDeTeclas.length ){
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+
+contador= contador + 1;
+
+console.log(contador);
+
+}
+
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;  
+}
